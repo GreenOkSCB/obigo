5d8a5fe783909c4e883b46d2ffcab61e054b76a3
GreenOkSCB <greenok.scb@gmail.com>
2022-10-27 16:14:02 +0300

    feat: adaptiv start

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 (function (){
+
     const header = document.querySelector('.header');
+    const header__wrapper = document.querySelector('.header__wrapper');
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
             header.classList.add('header_active');
+            header__wrapper.classList.add('header__wrapper_active');
         } else {
             header.classList.remove('header_active');
+            header__wrapper.classList.remove('header__wrapper_active');
         }
     }
+
 }());
